a14708ccbcbbd0a45163bbf0a06316f01e7f0e1f
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-04-14 12:54:06 +0200

    Add some changes Add 4 basic CRUD functions Add get_oldest_and_youngest function Add basic terminal printing

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -3,23 +3,55 @@ from view import terminal as view
 
 
 def list_employees():
-    view.print_error_message("Not implemented yet.")
+    list_of_employes = hr.read_content_from_file_in_nested_list()
+    line_of_text = "Nr."
+    for i in range(len(hr.HEADERS)):
+        line_of_text += " {:>12} ".format(hr.HEADERS[i])
+    print(line_of_text)
+    view.print_table(list_of_employes)
 
 
 def add_employee():
-    view.print_error_message("Not implemented yet.")
+    name = input("Please input name of employee: ")
+    birth_date = input("Please input birth date: ")
+    department = input("Please input department: ")
+    clearance = input("Please input clearance: ")
+    entry = [name, birth_date, department, clearance]
+    hr.append_nested_list_and_write_content(entry)
 
 
 def update_employee():
-    view.print_error_message("Not implemented yet.")
+    number = input("Please input number of employee: ")
+    name = input("Please input new name of employee: ")
+    birth_date = input("Please input new birth date: ")
+    department = input("Please input new department: ")
+    clearance = input("Please input new clearance: ")
+    entry = [name, birth_date, department, clearance]
+    hr.update_nested_list_and_write_content(number, entry)
 
 
 def delete_employee():
-    view.print_error_message("Not implemented yet.")
+    number = input("Please input number of employee: ")
+    hr.delete_nested_list_and_write_content(number)
 
 
 def get_oldest_and_youngest():
-    view.print_error_message("Not implemented yet.")
+    label = ("Oldest person:", "Youngest person:")
+    list_of_employes = hr.read_content_from_file_in_nested_list()
+    name = 1
+    birth_date = 2
+    list_of_birth_datas = []
+    for employee in list_of_employes:
+        list_of_birth_datas.append(employee[birth_date])
+    max_data = max(list_of_birth_datas)
+    min_data = min(list_of_birth_datas)
+    for employee in list_employees:
+        if max_data == employee[birth_date]:
+            oldest_name = employee[name]
+        if min_data == employee[birth_date]:
+            youngest_name = employee[name]
+    results = (oldest_name, youngest_name)
+    view.print_general_results(results, label)
 
 
 def get_average_age():
--- model/hr/hr.py
+++ model/hr/hr.py
@@ -12,3 +12,36 @@ from model import data_manager, util
 
 DATAFILE = "model/hr/hr.csv"
 HEADERS = ["Id", "Name", "Date of birth", "Department", "Clearance"]
+
+
+def read_content_from_file_in_nested_list():
+    return data_manager.read_table_from_file(DATAFILE, separator=';')
+
+
+def append_nested_list_and_write_content(entry):
+    data = data_manager.read_table_from_file(DATAFILE, separator=';')
+    name, birth_date, department, clearance = entry
+    data_to_append = [util.generate_id(), name, birth_date, department, clearance]
+    data.append(data_to_append)
+    write_to_file(data)
+
+
+def delete_nested_list_and_write_content(number):
+    data = data_manager.read_table_from_file(DATAFILE, separator=';')
+    del data[int(number) - 1]
+    write_to_file(data)
+
+
+def update_nested_list_and_write_content(number, entry):
+    index_of_employee = int(number) - 1
+    list_of_employes = data_manager.read_table_from_file(DATAFILE, separator=';')
+    name, birth_date, department, clearance = entry
+    list_of_employes[index_of_employee][1] = name
+    list_of_employes[index_of_employee][2] = birth_date
+    list_of_employes[index_of_employee][3] = department
+    list_of_employes[index_of_employee][4] = clearance
+    write_to_file(list_of_employes)
+
+
+def write_to_file(data):
+    return data_manager.write_table_to_file(DATAFILE, data, separator=';')
