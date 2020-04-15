db7c88be583f9ddac268931d17f70ea5a16b9cec
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2020-04-15 11:06:53 +0200

    Added generate id()

--- model/util.py
+++ model/util.py
@@ -7,4 +7,16 @@ def generate_id(number_of_small_letters=4,
                 number_of_digits=2,
                 number_of_special_chars=2,
                 allowed_special_chars=r"_+-!"):
-    return 'T!uq6-b4Yq'
+    random_id = ""
+    for i in range(int(number_of_small_letters)):
+        random_id +=random.choice(string.ascii_lowercase)
+    for i in range(int(number_of_capital_letters)):
+        random_id +=random.choice(string.ascii_uppercase)
+    for i in range(int(number_of_digits)):
+        random_id +=random.choice(string.digits)
+    for i in range(int(number_of_special_chars)):
+        random_id +=random.choice(allowed_special_chars)
+
+    random_id = ''.join(random.sample(random_id, len(random_id)))
+    
+    return random_id
