816a2569134e5fdc5bc9b0ae2216a476a814de38
marcinchaluda <chaluda@interia.pl>
2020-04-15 10:19:51 +0200

    separate write to a file and read from file

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -14,40 +14,48 @@ DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
 
 
+def read_data_from_file():
+    return data_manager.read_table_from_file(DATAFILE)
+
+
+def write_data_to_file(data):
+    return data_manager.write_table_to_file(DATAFILE, data)
+
+
 def list_transactions():
-    transactions = data_manager.read_table_from_file(DATAFILE)
+    transactions = read_data_from_file()
     transactions.insert(0, HEADERS)
     return transactions
 
 
 def add_transaction(entry):
-    transactions = data_manager.read_table_from_file(DATAFILE)
+    transactions = read_data_from_file()
     product, price, date = entry
     new_transaction = [util.generate_id(), util.generate_id(), product, price, date]
     transactions.append(new_transaction)
-    data_manager.write_table_to_file(DATAFILE, transactions)
+    write_data_to_file(transactions)
 
 
 def update_transaction(number, entry):
     index_of_transaction = int(number) - 1
-    transactions = data_manager.read_table_from_file(DATAFILE)
+    transactions = read_data_from_file()
     id, customer, product, price, date = entry
     transactions[index_of_transaction][0] = id
     transactions[index_of_transaction][1] = customer
     transactions[index_of_transaction][2] = product
     transactions[index_of_transaction][3] = price
     transactions[index_of_transaction][4] = date
-    data_manager.write_table_to_file(DATAFILE, transactions)
+    write_data_to_file(transactions)
 
 
 def remove_transaction(number):
-    transactions = data_manager.read_table_from_file(DATAFILE)
+    transactions = read_data_from_file()
     del transactions[int(number) - 1]
-    data_manager.write_table_to_file(DATAFILE, transactions)
+    write_data_to_file(transactions)
 
 
 def biggest_revenue_transaction():
-    transactions = data_manager.read_table_from_file(DATAFILE)
+    transactions = read_data_from_file()
     price_index = HEADERS.index("Price")
     transaction_price = [float(transaction[price_index]) for transaction in transactions]
     transaction_index = transaction_price.index(max(transaction_price))
